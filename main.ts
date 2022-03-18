a430bed3eee694f00df22e1b2a2b01d579fce45c
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-03-17 21:24:30 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    angle_threshold += 5
+    basic.showNumber(angle_threshold)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(head_count)
+})
+input.onButtonPressed(Button.B, function () {
+    angle_threshold += -5
+    basic.showNumber(angle_threshold)
+})
+let angle_threshold = 0
 let head_count = 0
+head_count = 0
 let current_head_down_time = 0
+angle_threshold = 100
 basic.forever(function () {
     if (input.rotation(Rotation.Pitch) > 100) {
         current_head_down_time += 1
